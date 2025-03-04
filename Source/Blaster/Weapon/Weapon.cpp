62f9d75aea13551b97b50521137a0c0346799e43
Emmanuel Reyre <magma242@gmail.com>
2025-03-04 16:22:22 +0100

    Lecture 45

--- Source/Blaster/Weapon/Weapon.cpp
+++ Source/Blaster/Weapon/Weapon.cpp
@@ -4,7 +4,9 @@
 #include "Blaster/Weapon/Weapon.h"
 
 #include "MovieSceneSection.h"
+#include "Blaster/Character/BlasterCharacter.h"
 #include "Components/SphereComponent.h"
+#include "Components/WidgetComponent.h"
 
 // Sets default values
 AWeapon::AWeapon()
@@ -26,19 +28,39 @@ AWeapon::AWeapon()
 	AreaSphere->SetCollisionResponseToAllChannels(ECR_Ignore);
 	AreaSphere->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 
+	PickupWidget = CreateDefaultSubobject<UWidgetComponent> (TEXT("PickupWidget"));
+	PickupWidget->SetupAttachment(RootComponent);
+
 }
 
 // Called when the game starts or when spawned
 void AWeapon::BeginPlay()
 {
 	Super::BeginPlay();
+	if (PickupWidget)
+	{
+		PickupWidget->SetVisibility(false);
+	}
 
 	if (HasAuthority())
 	{
 		AreaSphere->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
 		AreaSphere->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+		AreaSphere->OnComponentBeginOverlap.AddDynamic(this, &AWeapon::OnSphereOverlap);
+
+		
 	}
 	
+}
+
+void AWeapon::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	ABlasterCharacter* BlasterCharacter = Cast<ABlasterCharacter>(OtherActor);
+	if (BlasterCharacter && PickupWidget)
+	{
+		PickupWidget->SetVisibility(true);
+	}
 }
 
 // Called every frame
