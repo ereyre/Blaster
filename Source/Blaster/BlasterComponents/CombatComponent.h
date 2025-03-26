4717fdb2c162de8ed3869231df6c39eca49c7570
Emmanuel Reyre <magma242@gmail.com>
2025-03-26 15:55:42 +0100

    Lecture 77 : Replicating the Hit Target (RPC Fvector parameter)

--- Source/Blaster/BlasterComponents/CombatComponent.h
+++ Source/Blaster/BlasterComponents/CombatComponent.h
@@ -40,10 +40,10 @@ protected:
 	void FireButtonPressed(bool bPressed);
 
 	UFUNCTION(Server, Reliable)
-	void ServerFire();
+	void ServerFire(const FVector_NetQuantize TraceHitTarget );
 
 	UFUNCTION(NetMulticast, Reliable)
-	void MulticastFire();
+	void MulticastFire(const FVector_NetQuantize TraceHitTarget);
 
 
 	void TraceUnderCrosshairs(FHitResult& TraceHitResult);
@@ -66,6 +66,6 @@ private:
 	
 	bool bFireButtonPressed;
 
-	FVector HitTarget;
+	
 		
 };
