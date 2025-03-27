5a96286605b5d07674c764fcd58d1c6bb0163ace
Emmanuel Reyre <magma242@gmail.com>
2025-03-27 18:01:51 +0100

    Lecture 83 : Drawing crosshaire

--- Source/Blaster/HUD/BlasterHUD.h
+++ Source/Blaster/HUD/BlasterHUD.h
@@ -33,6 +33,7 @@ public:
 private:
 	FHUDPackage HUDPackage;
 
+	void DrawCrosshairs(UTexture2D* Texture, FVector2D ViewportCenter);
 
 public:
 	FORCEINLINE void SetHUDPackage(const FHUDPackage& InHUDPackage) { this->HUDPackage = InHUDPackage; }
